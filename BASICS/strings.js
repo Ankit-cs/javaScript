fecefa9c67462041cf050921422728c65bb8a0ce
Ankit-cs <ankitcareer018@gmail.com>
2024-10-29 10:49:42 +0000

    also some begginer friendly concepts

---
+++ BASICS/strings.js
@@ -0,0 +1,27 @@
+const name="Ankit Kumar"
+const repo_count=69
+//console.log(name+repo_count+" iloveit");
+console.log(`Hello My name is ${name} and my repo count is ${repo_count}` );
+const gameName=new String('    tsunamiwarrior-ak-com   ')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase);
+console.log(gameName.charAt(5));
+console.log(gameName.trim());
+console.log(gameName.indexOf('w'));
+console.log(gameName.split('-'));
+const url="https://ankit.com/ak%20"
+console.log(url.replace('%20' , '--'));
+console.log(url.includes('archana'));
+console.log(gameName[0]);
+
+
+
+
+
+
+
+
+
+
---
+++ BASICS/types.js
@@ -0,0 +1,36 @@
+//PRIMITIVE
+// 7types
+// 1:String,number,boolean,null,undefined,symbol,BigInt
+const score=100
+const scorevalue=100.3
+const isLogged=false;
+const outsideTemp=null
+let useEmail;
+const id=Symbol('123')
+const ssiId=Symbol('123')
+console.log(id===ssiId);
+const bigNumber=3548678444444444n
+
+const heros=["shaktimaan","naagin","JohnSnow"]
+let myobj={
+name:"Ankit kumar",
+Age:18,
+}
+const myfunction=function(){
+console.log("HELLOWORLD")
+}
+let myYtname="craftyPanda"
+let anotehrNmae=myYtname
+anotehrname="learnignju"
+console.log(anotehrNmae);
+console.log(myYtname)
+
+let user1={
+email:"ankit@gmail.com",
+upi:"usserby@ybl"
+}
+let userTwo=user1
+userTwo.email="ankijuhjnijldjfldkafdjnfl@gmail.com"
+console.log(userTwo.email)
+console.log(user1.email)
+
