384d33d45980754d6326139d362a08caf506252f
Ankit-cs <ankitcareer018@gmail.com>
2024-11-01 15:16:53 +0000

    learning after two days gap

---
+++ BASICS/date.js
@@ -0,0 +1,29 @@
+let myDate=new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.getHours());
+// console.log(typeof myDate);
+// // let myCreateDate=new Date(2027,1,17,7,5)
+// let myCreateDate=new Date("01-14-2024")
+// console.log(myCreateDate.toLocaleString());
+
+let myTimestamp=Date.now();
+console.log(myTimestamp);
+console.log(Math.floor(Date.now/1000));
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ BASICS/num.js
@@ -0,0 +1,44 @@
+const score=400
+const balance=new Number(100)
+console.log(score);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const other=1111123.6955
+console.log(other.toPrecision(3));
+
+const hum=10000000000000
+console.log(hum.toLocaleString('en-IN'));
+const max=Number.MAX_VALUE
+const min=Number.MIN_VALUE
+// console.log(max);
+// console.log(min);
+//  console.log(Math);
+//  console.log(Math.abs(-69));
+//  console.log(Math.round(69.6969));
+//  console.log(Math.floor(69.3636566));
+//  console.log(Math.ceil(69.564564));
+//  console.log(Math.min(1,-598,-6974,48));
+//  console.log(Math.max(Number.MAX_SAFE_INTEGER,Number.MAX_SAFE_INTEGER));
+console.log(Math.random)
+console.log((Math.random()*10)+1);
+console.log((Math.random()*100)+1);
+console.log((Math.random()*1000)+1);
+ const min2=10;
+ const max2=20;
+ console.log(Math.floor(Math.random()*(max2-min2+1))+min);
+ 
+ 
+
+ 
+
+
+ 
+
+
+
+
+
+
+
