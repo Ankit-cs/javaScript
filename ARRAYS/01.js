7002d3d3f6bacfac58431e95e653a396ce693a1a
Ankit-cs <ankitcareer018@gmail.com>
2024-11-05 14:16:21 +0000

    learned array

---
+++ ARRAYS/01.js
@@ -0,0 +1,45 @@
+const myArr=[0,1,2,3,4,5]
+const marvel=["LOKI","DEADPOOL","DOOM"]
+const dc=["BATMAN","SUPERMAN","AQUAMEN"]
+let myArray=new Array(1,2,2,4,5)
+console.log(myArr[0]);
+myArr.push(69)
+console.log(myArr);
+
+myArr.push(79)
+console.log(myArr);
+
+myArr.pop()
+console.log(myArr);
+
+myArr.pop()
+console.log(myArr);
+
+myArr.unshift(5)
+console.log(myArr);
+
+myArr.shift()
+console.log(myArr);
+
+console.log(myArr.includes(69));
+console.log(myArr.indexOf());
+console.log(myArr.indexOf(3));
+const myarey=myArr.join()
+console.log(myarey);
+const neuiw=marvel.concat(dc)
+console.log(neuiw);
+const uio=marvel.push(dc)
+console.log(uio);
+
+const jui=[...marvel,...dc]
+console.log(jui);
+let score1=69
+let score2=565
+let score3=898
+ const yup=Array.of(score1,score2,score3)
+console.log(yup);
+
+
+
+
+
